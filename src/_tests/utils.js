63f51e1d92fa646600946a66b96535eb6d3129d1
Nil Vila <nilvilamar@gmail.com>
2018-11-25 13:06:13 +0100

    Removed old test

--- src/_tests/utils.js
+++ src/_tests/utils.js
@@ -1,7 +1,5 @@
 import ClassOption from '../index';
 import getType from '../helpers/getType';
-import { inRange } from '../helpers/inRange';
-import { isValid } from '../helpers/isValid';
 
 const options = new ClassOption();
 
@@ -9,18 +7,6 @@ const options = new ClassOption();
 export function typeTest(input, output) {
   test((output.charAt(0).toUpperCase() + output.slice(1)), () => {
     expect(getType(input)).toBe(output);
-  });
-}
-
-export function validTest(num, input, list, output) {
-  test(`Valid test ${num}`, () => {
-    expect(isValid(input, list)).toBe(output);
-  });
-}
-
-export function rangeTest(num, input, object, output) {
-  test(`Range test ${num}`, () => {
-    expect(inRange(input, object.min, object.max, object.step)).toBe(output);
   });
 }
 
